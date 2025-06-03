7843824b535a3cdbfba9f376a23a910cd4ee3f0d
Awake <52383201+Aw4k3@users.noreply.github.com>
2025-06-03 16:05:03 +0100

    Scripting

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 const speedDisplay    = document.getElementById("speedDisplay");
 const positionDisplay = document.getElementById("positionDisplay");
 const viewport        = document.getElementById("viewport");
+const unitSelector    = document.getElementById("unitSelect");
 const radiusOfEarth   = 6371000; // metres
 const degToRad        = Math.PI / 180;
 const sampleSize      = 4;
 let speed             = 0; // for testing
 let glowHue           = 240;
-let unit              = 0; // 0: km/h, 1: mph, 2: m/s
+let glowIntensity     = 2;
+let glowEffect        = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
+                         0 0 calc(${glowIntensity} *  0.6rem) #fff,
+                         0 0 calc(${glowIntensity} *    1rem) #fff,
+                         0 0 calc(${glowIntensity} * 1.25rem)  hsl(${glowHue}, 100%, 50%),
+                         0 0 calc(${glowIntensity} *  1.8rem)  hsl(${glowHue}, 100%, 50%),
+                         0 0 calc(${glowIntensity} *  2.5rem)  hsl(${glowHue}, 100%, 50%),
+                         0 0 calc(${glowIntensity} *  3.5rem)  hsl(${glowHue}, 100%, 50%),
+                         0 0 calc(${glowIntensity} *  4.6rem)  hsl(${glowHue}, 100%, 50%);`
+let Units              = Object.freeze({
+                                        KMPH: Symbol(0),
+                                        MPH:  Symbol(1),
+                                        MPS:  Symbol(2)
+                                      });
+let unit              = 1;
 let coords            = [];
 let totalDistance     = 0;
+
+function setAtmosphereColour(hue) {
+  if (hue < 0) hue = 0;
+  if (hue > 359) hue = 359;
+  speedDisplay.style.textShadow    = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
+                                      0 0 calc(${glowIntensity} *  0.6rem) #fff,
+                                      0 0 calc(${glowIntensity} *    1rem) #fff,
+                                      0 0 calc(${glowIntensity} * 1.25rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  1.8rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  2.5rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  3.5rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  4.6rem)  hsl(${hue}, 100%, 50%);`;
+  positionDisplay.style.textShadow = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
+                                      0 0 calc(${glowIntensity} *  0.6rem) #fff,
+                                      0 0 calc(${glowIntensity} *    1rem) #fff,
+                                      0 0 calc(${glowIntensity} * 1.25rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  1.8rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  2.5rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  3.5rem)  hsl(${hue}, 100%, 50%),
+                                      0 0 calc(${glowIntensity} *  4.6rem)  hsl(${hue}, 100%, 50%);`;
+  viewport.style.background        = `radial-gradient(at top,
+                                      hsl(${hue}, 100.00%, 50.00%) 0%,
+                                    hsl(0, 0.00%, 0.00%)  80%);`
+}
 
 /**
  * 
@@ -21,8 +60,9 @@ function success(position) {
 }
 
 function failure() {
-  speedDisplay.innerHTML = "0";
-  positionDisplay.innerHTML = "Location unavailable";  
+  speedDisplay.innerHTML = "---";
+  positionDisplay.innerHTML = "Location unavailable";
+  setAtmosphereColour(0);
 }
 
 if (navigator.geolocation) {
@@ -39,11 +79,31 @@ if (navigator.geolocation) {
       totalDistance += radiusOfEarth * degToRad * Math.sqrt(Math.pow(Math.cos(latitude1 * degToRad ) * (longitude1 - longitude2), 2) + Math.pow(latitude1 - latitude2, 2));
     }
     
-    console.log(coords);
-    console.log(`${totalDistance / sampleSize}`);
+    // console.log(coords);
+    // console.log(`${totalDistance / sampleSize}`);
 
-    speedDisplay.innerHTML = Math.round(totalDistance / 1000);
+    switch (unit) {
+      case Units.KMPH:
+        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) / 1000);
+        break;
+    
+      case Units.MPH:
+        // update formula from mps to mph
+        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) * 2.237);
+        break;
 
+      case Units.MPS:
+        speedDisplay.innerHTML = Math.round(totalDistance / sampleSize);
+        break;
+
+      default:
+        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) * 2.237);
+        unitSelector.value = "1";
+        break;
+
+    }
+    
+    setAtmosphereColour(240);
     coords = [];
   }, 1000)
 }
