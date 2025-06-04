be72255b20bd4ad88e55870370d09f5d7585e628
Awake <52383201+Aw4k3@users.noreply.github.com>
2025-06-04 22:43:14 +0100

    Tweaked Calculations

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const speedDisplay    = document.getElementById("speedDisplay");
-const alert           = document.getElementById("alert");
 const viewport        = document.getElementById("viewport");
+const speedDisplay    = document.getElementById("speedDisplay");
+const alertDisplay    = document.getElementById("alertDisplay");
 const unitSelector    = document.getElementById("unitSelect");
 const radiusOfEarth   = 6371000; // metres
-const degToRad        = Math.PI / 180;
-const sampleSize      = 4;
-const sampleTime      = 1000;
-let speed             = 0; // for testing
+const degToRad        = Math.PI / 180; // pre-calculate
 let glowHue           = 240;
 let glowIntensity     = 2;
 let glowEffect        = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
@@ -18,13 +15,16 @@ let glowEffect        = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
                          0 0 calc(${glowIntensity} *  3.5rem)  hsl(${glowHue}, 100%, 50%),
                          0 0 calc(${glowIntensity} *  4.6rem)  hsl(${glowHue}, 100%, 50%);`
 let Units              = Object.freeze({
-                                        KMPH: Symbol(0),
-                                        MPH:  Symbol(1),
-                                        MPS:  Symbol(2)
+                                        KMPH: Number(0),
+                                        MPH:  Number(1),
+                                        MPS:  Number(2)
                                       });
 let unit              = 1;
-let coords            = [];
-let totalDistance     = 0;
+/** @type {GeolocationCoordinates} */
+let coords1           = null;
+/** @type {GeolocationCoordinates} */
+let coords2           = null;
+let distance     = 0;
 
 function setAtmosphereColour(hue) {
   if (hue < 0) hue = 0;
@@ -37,7 +37,7 @@ function setAtmosphereColour(hue) {
                                       0 0 calc(${glowIntensity} *  2.5rem)  hsl(${hue}, 100%, 50%),
                                       0 0 calc(${glowIntensity} *  3.5rem)  hsl(${hue}, 100%, 50%),
                                       0 0 calc(${glowIntensity} *  4.6rem)  hsl(${hue}, 100%, 50%);`;
-  alert.style.textShadow           = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
+  alertDisplay.style.textShadow           = `0 0 calc(${glowIntensity} *  0.3rem) #fff,
                                       0 0 calc(${glowIntensity} *  0.6rem) #fff,
                                       0 0 calc(${glowIntensity} *    1rem) #fff,
                                       0 0 calc(${glowIntensity} * 1.25rem)  hsl(${hue}, 100%, 50%),
@@ -55,8 +55,10 @@ function setAtmosphereColour(hue) {
  * @param {GeolocationPosition} position 
  */
 function success(position) {
-  if (coords.length < sampleSize) coords.push([position.coords.latitude, position.coords.longitude]);
-  alert.innerHTML = "Live";
+  coords1 = coords2 || position.coords;
+  coords2 = position.coords;
+  calculateSpeed();
+  alertDisplay.innerHTML = "Live";
 }
 
 /**
@@ -65,71 +67,78 @@ function success(position) {
  */
 function failure(err) {
   speedDisplay.innerHTML = "---";
-  alert.innerHTML = "Location unavailable";
-  console.error(`ERROR(${err.code}): ${err.message}`);
+  alertDisplay.innerHTML = err.message;
   setAtmosphereColour(0);
 }
 
-if (navigator.geolocation) {
-  setInterval(() => {
-    navigator.geolocation.getCurrentPosition(success, failure, { enableHighAccuracy: true });
-  }, sampleTime / sampleSize);
+function calculateSpeed() {    
+  // coords[lat, long, timestamp]
+  let latitude1  = coords1.latitude * degToRad;
+  let longitude1 = coords1.longitude * degToRad;
+  let latitude2  = coords2.latitude * degToRad;
+  let longitude2 = coords2.longitude * degToRad;
 
-  setInterval(() => {
-    totalDistance = 0;
-    
-    for (let i = 0; i < coords.length - 2; i++) {
-      // coords[lat, long]
-      let latitude1  = coords[i][0] * degToRad;
-      let longitude1 = coords[i][1] * degToRad;
-      let latitude2  = coords[i + 1][0] * degToRad;
-      let longitude2 = coords[i + 1][1] * degToRad;
+  // p
+  let rho1 = radiusOfEarth * Math.cos(latitude1);
+  let z1 = radiusOfEarth * Math.sin(latitude1);
+  let x1 = rho1 * Math.cos(longitude1);
+  let y1 = rho1 * Math.sin(longitude1);
 
-      // p
-      let rho1 = radiusOfEarth * Math.cos(latitude1);
-      let z1 = radiusOfEarth * Math.sin(latitude1);
-      let x1 = rho1 * Math.cos(longitude1);
-      let y1 = rho1 * Math.sin(longitude1);
+  // q
+  let rho2 = radiusOfEarth * Math.cos(latitude2);
+  let z2 = radiusOfEarth * Math.sin(latitude2);
+  let x2 = rho2 * Math.cos(longitude2);
+  let y2 = rho2 * Math.sin(longitude2);
 
-      // q
-      let rho2 = radiusOfEarth * Math.cos(latitude2);
-      let z2 = radiusOfEarth * Math.sin(latitude2);
-      let x2 = rho2 * Math.cos(longitude2);
-      let y2 = rho2 * Math.sin(longitude2);
+  // dot product
+  let dot = x1 * x2 + y1 * y2 + z1 * z2;
+  let cosTheta = dot / (radiusOfEarth ** 2);
 
-      // dot product
-      let dot = x1 * x2 + y1 * y2 + z1 * z2;
-      let cosTheta = dot / (radiusOfEarth ** 2);
-      let theta = Math.acos(cosTheta);
+  // clamp to prevent floating point errors
+  if (cosTheta > 1) cosTheta = 1;
+  if (cosTheta < 0) cosTheta = 0;
 
-      // distance in metres
-      totalDistance += radiusOfEarth * theta;
-    }
-    
-    console.log(`${totalDistance} / ${sampleSize} = ${totalDistance / sampleSize}`);
+  // complete dot product
+  let theta = Math.acos(cosTheta);
+  
+  // distance in metres
+  distance = radiusOfEarth * theta;
 
-    switch (unit) {
-      case Units.KMPH:
-        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) / 1000);
-        break;
-    
-      case Units.MPH:
-        // update formula from mps to mph
-        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) * 2.237);
-        break;
+  console.log(`${distance}`);
+  switch (unit) {
+    case Units.KMPH:
+      speedDisplay.innerHTML = Math.round(distance * 3.6);
+      break;
+  
+    case Units.MPH:
+      // update formula from mps to mph
+      speedDisplay.innerHTML = Math.round(distance * 2.237);
+      break;
+      
+    case Units.MPS:
+      speedDisplay.innerHTML = Math.round(distance);
+      break;
 
-      case Units.MPS:
-        speedDisplay.innerHTML = Math.round(totalDistance / sampleSize);
-        break;
-
-      default:
-        speedDisplay.innerHTML = Math.round((totalDistance / sampleSize) * 2.237);
-        unitSelector.value = "1";
-        break;
-
-    }
-    
-    setAtmosphereColour(240);
-    coords = [];
-  }, sampleTime)
+    default:
+      speedDisplay.innerHTML = Math.round(distance * 2.237);
+      unitSelector.value = "1";
+      break;
+  }
+  
+  setAtmosphereColour(240);
 }
+
+/**
+ * 
+ * @param {Event} e 
+ */
+function handleUnitChange(e) {
+  unit = Number(e.target.value);
+  console.log(unit);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(success, failure, { enableHighAccuracy: true });
+}
+
+unitSelector.addEventListener("change", handleUnitChange);
